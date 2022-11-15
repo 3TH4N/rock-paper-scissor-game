71c02a3942e4f2699b392a944423741132d81472
ElPuente <arauzjai@gmail.com>
2022-11-15 16:52:33 -0500

    need to fix this

---
+++ rps.js
@@ -0,0 +1,25 @@
+const playerOneScore = 0;
+const playerTwoScore = 0;
+
+const playerOneSpan = document.querySelector(".player-one")
+const playerTwoSpan = document.querySelector(".player-two")
+const winnerResult = document.querySelector(".winner")
+
+const squirtleDiv = document.getElementById("squirtle")
+const chimcharDiv = document.getElementById("chimchar")
+const bulbasaurDiv = document.getElementById("bulbasaur")
+
+
+function main() {
+    squirtleDiv.addEventListener('click', function(){
+
+    })
+
+    chimcharDiv.addEventListener('click', function(){
+        
+    })
+
+    bulbasaurDiv.addEventListener('click', function(){
+        
+    })
+}
